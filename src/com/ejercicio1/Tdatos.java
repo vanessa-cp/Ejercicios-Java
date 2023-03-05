c551fc8e6d3b1cef481211dcada7e1fce1f28108
vanessa.cp <vanehom@hotmail.com>
2023-03-05 18:50:19 +0100

    Ejercicio 1

---
+++ src/com/ejercicio1/Tdatos.java
@@ -0,0 +1,27 @@
+package com.ejercicio1;
+
+public class Tdatos {
+    public static void main(String[] args) {
+
+
+        int velocidad = 90;
+        System.out.println(velocidad);
+
+
+        long numTelefono = 1235687;
+        System.out.println(numTelefono);
+
+        double peso = 10.3d;
+        System.out.println(peso);
+
+        boolean serie1 = true;
+        System.out.println(serie1);
+
+        String nombre = "Vanessa";
+        System.out.println(nombre);
+
+
+
+
+    }
+}

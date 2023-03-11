26444064b50045c3092961f0c94c67c61731d639
vanessa.cp <vanehom@hotmail.com>
2023-03-11 20:44:11 +0100

    Ejercicio 3

--- src/com/ejercicio1/Ejercicio2.java
+++ src/com/ejercicio1/Ejercicio2.java
@@ -1,12 +1,38 @@
 package com.ejercicio1;
 
+import java.util.Scanner;
+
 public class Ejercicio2 {
 
-    static double getPrice(double precio){
-        return precio * 1.21;
+    public static  double calcularIVA(double p) {
+        double IVA;
+        IVA = p * 1.21;
+        return IVA;
+
+
+    }
+    public static void main(String[] args) {
+        Scanner lector = new Scanner(System.in);
+
+        double precioDeLaCompra, IVA;
+
+        System.out.println("Ingrese el precio de la compra");
+        precioDeLaCompra = lector.nextDouble();
+
+        IVA = calcularIVA(precioDeLaCompra);
+
+        System.out.println("El precio total sin  IVA es de : " +precioDeLaCompra);
+        System.out.println("El precio a pagar (IVA incluido) es de : " +(precioDeLaCompra+IVA));
+
+
+
 
     }
 
 
 
-}
+    }
+
+
+
+
---
+++ src/com/ejercicio1/Ejercicio3.java
@@ -0,0 +1,12 @@
+package com.ejercicio1;
+
+public class Ejercicio3 {
+    public static void main(String[] args) {
+
+        String[] nombres = {"Juan", "Pedro", "Maria", "Jose "};
+        for (int i = 0; i < nombres.length; i++) {
+            System.out.println(nombres[i]);
+        }
+
+    }
+}
